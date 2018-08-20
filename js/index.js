f8f14f832d200e58e6c68a0145ea375b4e95aaf7
jawakar <jawakarsmith@gmail.com>
2018-08-21 02:09:24 +0530

    added prepostion-remover
    added preposition remover->> ans words counter->> still needs precision

---
+++ js/index.js
@@ -0,0 +1,128 @@
+$(document).ready(function($) {
+
+    var prepositions = [
+      "a",
+      "abaft",
+      "aboard",
+      "about",
+      "above",
+      "absent",
+      "across",
+      "afore",
+      "after",
+      "against",
+      "along",
+      "alongside",
+      "amid",
+      "amidst",
+      "among",
+      "amongst",
+      "an",
+      "anenst",
+      "apropos",
+      "apud",
+      "around",
+      "as",
+      "aside",
+      "astride",
+      "at",
+      "athwart",
+      "atop",
+      "barring",
+      "before",
+      "behind",
+      "below",
+      "beneath",
+      "beside",
+      "besides",
+      "between",
+      "beyond",
+      "but",
+      "by",
+      "circa",
+      "concerning",
+      "despite",
+      "down",
+      "during",
+      "except",
+      "excluding",
+      "failing",
+      "following",
+      "for",
+      "forenenst",
+      "from",
+      "given",
+      "in",
+      "including",
+      "inside",
+      "into",
+      "lest",
+      "like",
+      "mid",
+      "midst",
+      "minus",
+      "modulo",
+      "near",
+      "next",
+      "notwithstanding",
+      "of",
+      "off",
+      "on",
+      "onto",
+      "opposite",
+      "out",
+      "outside",
+      "over",
+      "pace",
+      "past",
+      "per",
+      "plus",
+      "pro",
+      "qua",
+      "regarding",
+      "round",
+      "sans",
+      "save",
+      "since",
+      "than",
+      "through",
+      "throughout",
+      "till",
+      "times",
+      "to",
+      "toward",
+      "towards",
+      "under",
+      "underneath",
+      "unlike",
+      "until",
+      "unto",
+      "up",
+      "upon",
+      "versus",
+      "via",
+      "vice",
+      "with",
+      "within",
+      "without",
+      "worth"
+    ]
+
+    var wordsAndCount = {};
+
+    //loop through each element and change the state
+    i2.forEach((element)=>{
+            var preportionRemoved = element.innerText.split(" ").map((n)=>{
+                if(prepositions.indexOf(n) > -1){
+                return n.replace(n,"");
+            }else{
+                return n;
+            }
+        });
+        element.innerText = preportionRemoved.join(" ");
+        console.log(element.innerText.replace(/,/,""));
+    });
+
+
+
+});
